834c3ecd11cb69f871d380d844df5eba9be30d68
Lkey19971110 <a093053868131@gmail.com>
2023-10-11 02:25:22 +0800

    Update 完成左右移動與解決角色一直往下掉

--- Assets/Scripts/Controlsystem.cs
+++ Assets/Scripts/Controlsystem.cs
@@ -9,7 +9,31 @@ namespace Lkey.TwoD
     /// </summary>
     public class C : MonoBehaviour
     {
+        [SerializeField, Header("移動速度"), Range(0, 500)]
+        private float moveSpeed = 3.5f;
 
+        private Rigidbody2D rig;
+
+        private void Awake()
+        {
+           // print("<color=yellow>喚醒事件</color>");
+
+            rig = GetComponent<Rigidbody2D>();
+        }
+        private void Start()
+        {
+            //print("<color=yellow>開始事件</color>");
+        }
+        private void Update()
+        {
+            //print("<color=yellow>更新事件</color>");
+
+            //float v = Input.GetAxis("Vertiacl");
+            //print("<color=#f66>垂直的值:" + v + "</color>");
+
+            float h = Input.GetAxis("Horizontal");
+            rig.velocity = new Vector2 (h* moveSpeed, rig.velocity.y);
+        }
     }
 
 }
