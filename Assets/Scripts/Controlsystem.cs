2f4c23b5577bdaef10cd3bae1f8941ab587fb370
lkey1110 <138636171+lkey1110@users.noreply.github.com>
2023-10-11 09:07:03 +0800

    Update 追蹤玩家攝影機
    進度到攝影機 : 2D追蹤玩家攝影機結束

--- Assets/Scripts/Controlsystem.cs
+++ Assets/Scripts/Controlsystem.cs
@@ -9,16 +9,18 @@ namespace Lkey.TwoD
     /// </summary>
     public class C : MonoBehaviour
     {
-        [SerializeField, Header("移動速度"), Range(0, 500)]
+        [SerializeField, Header("移動速度"), Range(0, 50)]
         private float moveSpeed = 3.5f;
 
         private Rigidbody2D rig;
-
+        private Animator ani;
+        private string parRun = "on/off run";
         private void Awake()
         {
            // print("<color=yellow>喚醒事件</color>");
 
             rig = GetComponent<Rigidbody2D>();
+            ani = GetComponent<Animator>();
         }
         private void Start()
         {
@@ -28,11 +30,32 @@ namespace Lkey.TwoD
         {
             //print("<color=yellow>更新事件</color>");
 
+            MoveAndFlip();
+        }
+
+        /// <summary>
+        /// 移動與翻面
+        /// </summary>
+        private void MoveAndFlip()
+        {
             //float v = Input.GetAxis("Vertiacl");
             //print("<color=#f66>垂直的值:" + v + "</color>");
 
             float h = Input.GetAxis("Horizontal");
-            rig.velocity = new Vector2 (h* moveSpeed, rig.velocity.y);
+            rig.velocity = new Vector2(h * moveSpeed, rig.velocity.y);
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                print("按下A");
+                transform.eulerAngles = Vector3.zero;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                print("按下D");
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+
+            ani.SetBool(parRun, h != 0);
         }
     }
 
