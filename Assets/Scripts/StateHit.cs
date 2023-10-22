8d187319998c4b230071a124e56ea83e2c147d9c
Lkey19971110 <a093053868131@gmail.com>
2023-10-22 18:32:32 +0800

    Update 敵人受傷回到遊走狀態和面向玩家完成

--- Assets/Scripts/StateHit.cs
+++ Assets/Scripts/StateHit.cs
@@ -1,11 +1,59 @@
 ﻿using UnityEngine;
 namespace Lkey
 {
+    /// <summary>
+    /// 受傷狀態
+    /// </summary>
+    /// <returns></returns>
     public class StateHit : State
     {
+        private string parHit = "觸發受傷";
+        private bool isHit;
+        private float timeHit = 0.6f;
+        private float timer;
+
+        [SerializeField, Header("遊走狀態")]
+        private StateWander stateWander;
+
+        private Transform player;
+        private void Start()
+        {
+            player = GameObject.Find("太空員").transform;
+        }
         public override State RunCurrentState()
         {
+            if (!isHit)
+            {
+                isHit = true;
+                ani.SetTrigger(parHit);
+                FlipToPlayer();
+            }
+
+            if (isHit)
+            {
+                timer += Time.deltaTime;
+                if (timer > timeHit)
+                {
+                    isHit =false;
+                    return stateWander;
+                }
+            }
+
             return this;
+        }
+
+        private void FlipToPlayer()
+        {
+            if (transform.position.x > player.position.x)
+            {
+                stateWander.direction = -1;
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            else if (transform.position.x < player.position.x)
+            {
+                stateWander.direction = 1;
+                transform.eulerAngles = Vector3.zero;
+            }
         }
     }
 }
