2fd752cbdff5e1e11a17d4361be808b9e76826d1
Lkey19971110 <a093053868131@gmail.com>
2023-10-22 15:01:22 +0800

    Update 完成死亡回歸

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,56 @@
+﻿using Fungus;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Lkey
+{
+    /// <summary>
+    /// 玩家受傷
+    /// </summary>
+    public class DamagePlayer : DamageSystem
+    {
+        [SerializeField, Header("血條")]
+        private Image imgHp;
+        [SerializeField, Header("遊戲管理器")]
+        protected Flowchart fungusGM;
+        [SerializeField, Header("受傷著色器材質球")]
+        private Material mHurt;
+
+
+        private void OnEnable()
+        {
+            mHurt.SetFloat("_Hurt", 0);
+        }
+        private void OnDisable()
+        {
+            mHurt.SetFloat("_Hurt", 0);
+        }
+
+        public override void Damage(float getDamage)
+        {
+            base.Damage(getDamage); //父類別原有內容
+
+            imgHp.fillAmount = hp / hpMax;
+
+            StartCoroutine(DamageEffect());
+        }
+
+        protected override void Dead()
+        {
+            fungusGM.SendFungusMessage("遊戲失敗");
+            Destroy(gameObject);
+        }
+
+        private IEnumerator DamageEffect()
+        {
+
+            mHurt.SetFloat("_Hurt", 0.3f);
+            yield return new WaitForSeconds(0.1f);
+            mHurt.SetFloat("_Hurt", 0);
+            yield return new WaitForSeconds(0.1f);
+            
+        }
+    }
+}
+
--- Assets/Scripts/DamageSystem.cs
+++ Assets/Scripts/DamageSystem.cs
@@ -12,8 +12,8 @@ namespace Lkey
         [SerializeField, Header("傷害值文字預製物")]
         private GameObject prefabDamage;
 
-        private float hp;
-        private float hpMax;
+        protected float hp;
+        protected float hpMax;
 
         private void Awake()
         {
@@ -24,7 +24,7 @@ namespace Lkey
         /// 受傷
         /// </summary>
         /// <param name="getDamage">受到的傷害</param>
-        public void Damage(float getDamage)
+        public virtual void Damage(float getDamage)
         {
             hp -= getDamage;
 
@@ -37,7 +37,7 @@ namespace Lkey
         /// <summary>
         /// 死亡
         /// </summary>
-        private void Dead()
+        protected virtual void Dead()
         {
             
         }
