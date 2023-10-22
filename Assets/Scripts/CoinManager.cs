73182e2b603434fbff8158dfef4574c47ccefdc0
Lkey19971110 <a093053868131@gmail.com>
2023-10-22 20:46:02 +0800

    Update 完成吃金幣獲得勝利資料

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,37 @@
+﻿
+using Fungus;
+using TMPro;
+using UnityEngine;
+
+namespace Lkey
+{
+    public class CoinManager : MonoBehaviour
+    {
+        [SerializeField, Header("遊戲管理器")]
+        private Flowchart flowchatGM;
+
+        private TextMeshProUGUI textCoin;
+        private int coinCurrent;
+        private int coinTotal = 10;
+
+        private void Awake()
+        {
+            textCoin = GameObject.Find("金幣數量").GetComponent<TextMeshProUGUI>();
+            textCoin.text = $"金幣數量 : {coinCurrent}/{coinTotal}";
+        }
+
+        private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+        {
+            if (collision.gameObject.name.Contains("金幣")) EatCoin(collision.gameObject);
+        }
+        private void EatCoin(GameObject coin)
+        {
+            Destroy(coin);
+            textCoin.text = $"金幣數量:{++coinCurrent}/{coinTotal}";
+
+            if (coinCurrent >= coinTotal) flowchatGM.SendFungusMessage("遊戲勝利");
+        }
+    }
+
+}
+
